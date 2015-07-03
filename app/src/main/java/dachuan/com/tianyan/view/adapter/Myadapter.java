c23f34663c4ee42bd9bd783db92ef16f737f0990
lingshaojing <1542424346@qq.com>
2015-07-03 12:27:46 +0800

    缩放imageView mainactivity布局

---
+++ app/src/main/java/dachuan/com/tianyan/view/adapter/Myadapter.java
@@ -0,0 +1,53 @@
+package dachuan.com.tianyan.view.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import dachuan.com.tianyan.R;
+
+
+/**
+ * Created by linsj on 15-7-2.
+ */
+public class Myadapter extends RecyclerView.Adapter{
+
+    private  List<String> list;
+    public Myadapter(List<String> list) {
+        this.list = list;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.holder_test,null);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Myadapter.ViewHolder viewHolder = (ViewHolder)holder;
+        viewHolder.text.setText("Hello , " + list.get(position));
+        viewHolder.image.setImageResource(R.mipmap.ic_launcher);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView text;
+        ImageView image;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            text = (TextView) itemView.findViewById(R.id.view_text);
+            image = (ImageView) itemView.findViewById(R.id.img);
+        }
+    }
+}
--- app/src/main/java/dachuan/com/tianyan/view/base/BaseActivity.java
+++ app/src/main/java/dachuan/com/tianyan/view/base/BaseActivity.java
@@ -6,6 +6,8 @@ import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 import butterknife.ButterKnife;
 import dachuan.com.tianyan.AppManager;
 import rx.Observable;
@@ -23,13 +25,17 @@ public  abstract  class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(getLayoutId());
         AppManager.getAppManager().addActivity(this);
         ButterKnife.bind(this);
         init(savedInstanceState);
     }
-    public abstract void init(Bundle savedInstanceState);
+
     public abstract int getLayoutId();
+
+    public abstract void init(Bundle savedInstanceState);
+
 
     @Override protected void onPause() {
         subscription.clear();
