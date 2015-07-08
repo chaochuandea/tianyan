6dbe499e9e2faedf3dec05cbbadafdb3c0a229bf
lingshaojing <1542424346@qq.com>
2015-07-08 11:41:47 +0800

    主页listitem动画

--- app/src/main/java/dachuan/com/tianyan/view/adapter/EveryDayAdapter.java
+++ app/src/main/java/dachuan/com/tianyan/view/adapter/EveryDayAdapter.java
@@ -1,13 +1,21 @@
 package dachuan.com.tianyan.view.adapter;
 
+import com.nineoldandroids.animation.AnimatorSet;
+
+import android.animation.StateListAnimator;
 import android.graphics.drawable.LayerDrawable;
 import android.graphics.drawable.RotateDrawable;
 import android.graphics.drawable.ScaleDrawable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +26,8 @@ import com.nineoldandroids.animation.ValueAnimator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import dachuan.com.tianyan.R;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -28,36 +38,83 @@ import timber.log.Timber;
 /**
  * Created by linsj on 15-7-2.
  */
-public class EveryDayAdapter extends RecyclerView.Adapter{
+public class EveryDayAdapter extends RecyclerView.Adapter {
 
-    private  List<String> list;
+
+    private float firstX;
+    private float firstY;
+    private boolean isFlipUp;
+    private List<String> list;
+
     public EveryDayAdapter(List<String> list) {
+
         this.list = list;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.holder_test,null);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.holder_test, null);
         return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ViewHolder viewHolder = (ViewHolder)holder;
-        viewHolder.name.setText("Hello , " + list.get(position));
-        LayerDrawable layerDrawable = (LayerDrawable) viewHolder.rotate.getDrawable();
-
-        RotateDrawable rottate = (RotateDrawable)layerDrawable.getDrawable(1);
-        ValueAnimator animator =  ObjectAnimator.ofInt(0, 10000, 0);
-        animator.setInterpolator(new AccelerateDecelerateInterpolator());
-        animator.addUpdateListener(animation -> {
-            int level = (int) (10000 * animation.getAnimatedFraction());
-            rottate.setLevel(level);
-            Timber.d("ssss", level + "");
+        ViewHolder viewHolder = (ViewHolder) holder;
+        viewHolder.rootView.setLongClickable(true);
+        viewHolder.rootView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                AnimatorSet animatorSet = new AnimatorSet();
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        firstX = event.getX();
+                        firstY = event.getY();
+                        animatorSet.playTogether(ObjectAnimator.ofFloat(viewHolder.textLayout, "alpha", 1.0f, 0.0f)
+                                .setDuration(800), ObjectAnimator.ofFloat(viewHolder.coverView, "alpha", 0.5f, 0.0f)
+                                .setDuration(800));
+                        animatorSet.start();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        float dx = firstX - event.getX();
+                        float dy = firstY - event.getY();
+                        if(dx == dy) break;
+                        else if (dx > dy) isFlipUp = false;
+                        else if (dx < dy) isFlipUp = true;
+                        if(isFlipUp)
+                        {
+                            animatorSet.playTogether(ObjectAnimator.ofFloat(viewHolder.textLayout, "alpha", 0.0f, 1.0f)
+                                    .setDuration(800), ObjectAnimator.ofFloat(viewHolder.coverView, "alpha", 0.0f, 0.5f)
+                                    .setDuration(800));
+                            animatorSet.start();
+                            return false;
+                        }
+                        else break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_OUTSIDE:
+                    case MotionEvent.ACTION_CANCEL:
+                        animatorSet.playTogether(ObjectAnimator.ofFloat(viewHolder.textLayout, "alpha", 0.0f, 1.0f)
+                                .setDuration(800), ObjectAnimator.ofFloat(viewHolder.coverView, "alpha", 0.0f, 0.5f)
+                                .setDuration(800));
+                        animatorSet.start();
+                        break;
+                }
+                return false;
+            }
         });
-        animator.setDuration(1000);
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.start();
+        viewHolder.name.setText("Hello , " + list.get(position));
+//        LayerDrawable layerDrawable = (LayerDrawable) viewHolder.rotate.getDrawable();
+//
+//        RotateDrawable rottate = (RotateDrawable) layerDrawable.getDrawable(1);
+//        ValueAnimator animator = ObjectAnimator.ofInt(0, 10000, 0);
+//        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+//        animator.addUpdateListener(animation -> {
+//            int level = (int) (10000 * animation.getAnimatedFraction());
+//            rottate.setLevel(level);
+//            Timber.d("ssss", level + "");
+//        });
+//        animator.setDuration(1000);
+//        animator.setRepeatCount(ValueAnimator.INFINITE);
+//        animator.start();
 
     }
 
@@ -66,18 +123,28 @@ public class EveryDayAdapter extends RecyclerView.Adapter{
         return list.size();
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder {
 
-        SimpleDraweeView cover,cover_bg;
-        ImageView rotate;
-        TextView name,tag_and_time;
+
+        @Bind(R.id.name)
+        TextView name;
+        @Bind(R.id.tag_and_time)
+        TextView tag_and_time;
+        @Bind(R.id.text_layout)
+        View textLayout;
+        @Bind(R.id.view_cover)
+        View coverView;
+
+        View rootView;
+
         public ViewHolder(View itemView) {
             super(itemView);
-            name = (TextView) itemView.findViewById(R.id.name);
-            tag_and_time = (TextView) itemView.findViewById(R.id.tag_and_time);
-            rotate = (ImageView) itemView.findViewById(R.id.rotate);
-//            cover = (SimpleDraweeView) itemView.findViewById(R.id.cover);
-//            cover_bg = (SimpleDraweeView) itemView.findViewById(R.id.cover_bg);
+            ButterKnife.bind(this, itemView);
+            this.rootView = itemView;
+
+//            name = (TextView) itemView.findViewById(R.id.name);
+//            tag_and_time = (TextView) itemView.findViewById(R.id.tag_and_time);
+//            rotate = (ImageView) itemView.findViewById(R.id.rotate);
         }
     }
 }
